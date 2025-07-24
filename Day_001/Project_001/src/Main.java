8f324a1dfa4912ab7abdf7258b41ef0f1bfb029c
ravihamidov <ravihamidov42@gmail.com>
2025-07-24 13:36:34 +0400

    Add: Introduction to Java & Variables

---
+++ Day_001/Project_001/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        // Introduction to Java
+        // This is my first Java Code
+        /*
+        This
+        is
+        multi
+        -
+        line
+        comment
+         */
+
+        // 001 with nothing
+        System.out.print("i like pizza so much!");
+        System.out.print("it's really good !");
+        // 002 with ln
+        System.out.println("i like Burger !");
+        System.out.println("it's really good !");
+        // 003 with \n
+        System.out.print("i like Shaurma !\n");
+        System.out.print("it's really good !\n");
+    }
+}
